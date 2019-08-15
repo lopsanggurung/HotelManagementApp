a92052d2b98c9a0f84ef4b8bb620d78c53bfcb96
lopsanggurung <lopsanggrg@gmail.com>
2019-08-15 16:47:43 -0400

    Defined Angular File Structure

---
+++ App-SPA/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './core/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'pages',
+    component: PagesComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
--- App-SPA/src/app/app.module.ts
+++ App-SPA/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 
@@ -12,7 +15,10 @@ import { ValueComponent } from './value/value.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      CoreModule,
+      AppRoutingModule,
+      PagesModule
    ],
    providers: [],
    bootstrap: [
